5d37bbab52fc7bb52a8f3a6b7ea136ef1aaa3b68
Davide Gessa <gessadavide@gmail.com>
2015-07-03 17:44:10 +0200

    messages

--- main.c
+++ main.c
@@ -9,19 +9,22 @@ int main ()
 
 	while (ch = getchar ())
 	{
-		if (ch == 'a')
+		if (ch == 'a') {
 			boat_turnleft ();
-		else if (ch == 'd')
+		}
+		else if (ch == 'd') {
 			boat_turnright ();
-		else if (ch == 's')
+		}
+		else if (ch == 's'){
 			boat_stop ();
-		else if (ch == 'w')
+		}
+		else if (ch == 'w'){
 			boat_gocenter ();
+		}
 		else if (ch == 'q'){
 			boat_stop ();
 			return 0;
 		}
-
 	}
 
 	return 0;
