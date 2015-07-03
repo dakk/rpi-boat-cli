0a79865c5778c057528515b3849ebdd759989b0d
Davide Gessa <gessadavide@gmail.com>
2015-07-03 17:17:33 +0200

    wp mode

--- main.c
+++ main.c
@@ -1,9 +1,26 @@
+#include <stdio.h>
+
 #include "boat.h"
 
 int main ()
 {
+	char ch = 's';
 	boat_init ();
-	boat_gocenter ();
+	boat_stop ();
+
+	while (ch = getchar ())
+	{
+		if (ch == 'a')
+			boat_turnleft ();
+		else if (ch == 'd')
+			boat_turnright ();
+		else if (ch == 'q')
+			return 0;
+		else if (ch == 's')
+			boat_stop ();
+		else if (ch == 'w')
+			boat_gocenter ();
+	}
 
 	return 0;
 }
